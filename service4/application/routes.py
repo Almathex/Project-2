d38b645a11d53a958bc7acd4799aaca20ac59cfc
Almathex <bradalexsmith@gmail.com>
2020-12-02 14:51:51 +0000

    service4

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -3,13 +3,13 @@ from flask import request, Response
 from random import randint
 
 @app.route('/prize1', methods = ['POST'])
-def prize_one():
-    prizeusername = request.data.decode('utf-8')
-    if prizename[0] == '3' or prizename[5] == 'L':
+def prize1():
+    amount = request.data.decode('utf-8')
+    if amount[0] == '3' or amount[4] == 'L':
         prize = 'You win £300'
-    elif prizename[0] == '5' or prizename[7] == 'o':
+    elif amount[0] == '5' or amount[4] == 'o':
         prize = 'You win £5000'
-    elif prizename[0] == '7' or prizename[4] == 'A':
+    elif amount[0] == '7' or amount[4] == 'A':
         prize = 'You win £70000'
     else: 
         prize = "Unfortunatly you didnt win this time!"
