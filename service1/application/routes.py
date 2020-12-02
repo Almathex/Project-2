ac4d8b831133cb38a4ff2ccef4c816fe62d8100f
Almathex <bradalexsmith@gmail.com>
2020-12-02 08:52:42 +0000

    nearly

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,17 +1,19 @@
 from application import app, db
-from flask import render_template, request, Response
+from flask import render_template, request, Response, redirect, url_for
 import requests
 from application.models import Prize
 
-@app.route('/', methods=['GET'])
+
+@app.route('/')
+@app.route('/home', methods=['GET'])
 def home():
     four_numbers = requests.get('http://service2:5001/generator/four_numbers')
     four_letters = requests.get('http://service3:5002/generator/four_letters')
     username = str(four_numbers.text)+four_letters.text
     return render_template('home.html',title='home', username=username)
 
-@app.route('/prize/<prizetoken>', methods-['GET', 'POST')]
-def prize(prizetoken):
+@app.route('/prize/<prizename>', methods=['GET', 'POST'])
+def prize(prizename):
     prize = requests.post('http://service4:5003/prize1', data=prizename)
     info = Prize(username=prizename, prize=prize.text)
     db.session.add(info)
