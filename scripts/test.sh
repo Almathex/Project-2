b049b57d43d0c92345cf906962048eaa6dbdd017
Almathex <bradalexsmith@gmail.com>
2020-12-03 10:59:13 +0000

    jenkinsfile

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,20 +3,17 @@ sudo apt update
 sudo apt install python3 python3-pip python3-venv
 python3 -m venv venv
 . venv/bin/activate
-cd service1
 pip3 install -r requirements.txt
+cd service1
 pytest --cov ./application
 cd ..
 cd service2
-pip3 install -r requirements.txt
 pytest --cov ./application
 cd ..
 cd service3
-pip3 install -r requirements.txt
 pytest --cov ./application
 cd ..
 cd service4
-pip3 install -r requirements.txt
 pytest --cov ./application
 
 deactivate
