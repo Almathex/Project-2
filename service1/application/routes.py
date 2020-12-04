bd1c70a7ceca84303f3591dc965425277a9fd14f
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-04 14:38:24 +0000

    sql

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -6,13 +6,13 @@ from application.models import prize
 
 @app.route('/', methods=['GET'])
 def home():
+    return render_template('home.html',title='home', string=string)
+
+@app.route('/prize/', methods=['GET', 'POST'])
+def prize():
     four_numbers = requests.get('http://service2:5001/generator/four_numbers')
     four_letters = requests.get('http://service3:5002/generator/four_letters')
     string = str(four_numbers.text)+four_letters.text
-    return render_template('home.html',title='home', string=string)
-
-@app.route('/prize/<amount>', methods=['GET', 'POST'])
-def prize(amount):
     prize = requests.post('http://service4:5003/prize1', data=amount)
     prizes = prize(
             random_string = str(amount),
