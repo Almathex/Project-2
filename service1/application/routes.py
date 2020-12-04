7da9d323f880d2c4f3135266163b92bdc12a4711
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-04 14:48:36 +0000

    sql

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -13,11 +13,11 @@ def prize():
     four_numbers = requests.get('http://service2:5001/generator/four_numbers')
     four_letters = requests.get('http://service3:5002/generator/four_letters')
     string = str(four_numbers.text)+four_letters.text
-    prize = requests.post('http://service4:5003/prize1', data=string)
+    prizetwo = requests.post('http://service4:5003/prize1', data=string)
     prizes = prize(
             random_string = str(string),
-            winnings = prize
+            winnings = prizetwo
     )    
     db.session.add(prizes)
     db.session.commit()
-    return render_template('prize.html', title='prize', string=string, prize=prize.text)
+    return render_template('prize.html', title='prize', string=string, prizetwo=prizetwo.text)
