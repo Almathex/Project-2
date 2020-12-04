85cc1bbea0f2acf55086aad9c0f7feefff1dcb6a
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-04 13:25:27 +0000

    changed

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-sudo su jenkins
-pip3 install ansible
-~/.local/bin/ansible-playbook -v -i inventory playbook.yaml
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:home/jenkins/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+pip3 install --user ansible
+/home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
