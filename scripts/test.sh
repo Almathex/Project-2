b398c2de1ce52fa69b9b727c1cfdb57eaa0d971c
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-04 11:12:05 +0000

    fixed test 4

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install python3 python3-pip python3-venv -y
 python3 -m venv venv
 . venv/bin/activate
+pip3 install pytest
 pip3 install -r requirements.txt
 cd service1
 pytest --cov ./application
