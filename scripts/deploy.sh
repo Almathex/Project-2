6a93f36f9e7b5776e7c4cf2c44722eac4affe6ab
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-05 16:12:52 +0000

    hmm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 ssh jenkins@project-manager -o StrictHostKeyChecking=no << EOF
-docker pull almathex/service1:latest
-docker pull almathex/service2:latest
-docker pull almathex/service3:latest
-docker pull almathex/service4:latest
-docker pull nginx:alpine
+sudo docker pull almathex/service1:latest
+sudo docker pull almathex/service2:latest
+sudo docker pull almathex/service3:latest
+sudo docker pull almathex/service4:latest
+sudo docker pull nginx:alpine
 git clone https://github.com/almathex/Project-2.git
 cd Project-2
 sudo docker stack deploy --compose-file docker-compose.yaml randprize
