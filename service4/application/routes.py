cf5b8072a03aed2804729be1d7b854f56140b032
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-06 20:42:15 +0000

    2nd iteration

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -15,14 +15,14 @@ from random import randint
 #        winning = "Unfortunatly you didnt win this time!"
 #    return Response(winning, mimetype= 'text/plain')
 @app.route('/prize2', methods = ['POST'])
- def prize2():
-   code = request.data.decode('utf-8')
-   if code[0] == '1' or code[4] == 'E':
+def prize2():
+    code = request.data.decode('utf-8')
+    if code[0] == '1' or code[4] == 'E':
        winning == 'You won a laptop!'
-   elif code[0] == '2':
+    elif code[0] == '2':
        winning == 'You won a car!'
-   elif code [0] == '4':
+    elif code [0] == '4':
        winning = 'You won a house!':
-   else:
+    else:
        winning = "Unfortunatly you didnt win this time!"
     return Response(winning, mimetype= 'text/plain')                
