e71b10704b1ed2979f9ee66a47812961ba956752
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-06 19:30:58 +0000

    make it easier to get 5000

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -7,7 +7,7 @@ def prize1():
     code = request.data.decode('utf-8')
     if code[0] == '3' or code[4] == 'L':
         winning = 'You win 300 pound'
-    elif code[4] == 'O':
+    elif code[4] == '9':
         winning = 'You win 5000 pound'
     elif code[0] == '7' and code[4] == 'A':
         winning = 'You win 70000 pound'
