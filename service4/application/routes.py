27f70c153e6b94fb9ecf3bdfda6a6ece99eb110c
Alex-Smith <smithalexanderbradley@gmail.com>
2020-12-06 15:41:09 +0000

    hm

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -4,12 +4,12 @@ from random import randint
 
 @app.route('/prize1', methods = ['POST'])
 def prize1():
-    amount = request.data.decode('utf-8')
-    if amount[0] == '3' or amount[4] == 'L':
+    code = request.data.decode('utf-8')
+    if code[0] == '3' or code[4] == 'L':
         winning = 'You win 300 pound'
-    elif amount[4] == 'O':
+    elif code[4] == 'O':
         winning = 'You win 5000 pound'
-    elif amount[0] == '7' and amount[4] == 'A':
+    elif code[0] == '7' and code[4] == 'A':
         winning = 'You win 70000 pound'
     else: 
         winning = "Unfortunatly you didnt win this time!"
